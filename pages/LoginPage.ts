4d8a5abc0b0f823c00aefb26d69e5eb99788653b
Richard Kim <richardk1026417@gmail.com>
2025-07-30 03:03:45 -0700

    Finished login.spec.ts & LoginPage.ts

---
+++ pages/LoginPage.ts
@@ -0,0 +1,40 @@
+import { Page } from '@playwright/test';
+
+/**
+ * This class goes to appropriate website, allows filling in 
+ * username & password, then clicks login
+ */
+export class LoginPage 
+{
+    readonly page: Page;
+
+    // Declare locators
+    readonly usernameField;
+    readonly passwordField;
+    readonly loginButton;
+    readonly errorMessage;
+
+    constructor(page: Page)
+    {
+        this.page = page;
+
+        // Locators
+        this.usernameField = page.locator('#user-name');
+        this.passwordField = page.locator('#password');
+        this.loginButton = page.locator('#login-button');
+        this.errorMessage = page.locator('[data-test="error"]')
+    }
+
+    // Go to the testing website
+    async gotoLoginPage()
+    {
+        await this.page.goto('/');
+    }
+
+    async login(username: string, password: string)
+    {
+        await this.usernameField.fill(username);
+        await this.passwordField.fill(password);
+        await this.loginButton.click();
+    }
+}
---
+++ tests/login.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+test.describe('Login Tests', () => 
+{
+    test('Valid login redirects to inventory page', async ({page}) =>
+    {
+        // Create a new object (login) 
+        const login = new LoginPage(page);
+
+        // Goes to page and fills username and password fields
+        await login.gotoLoginPage();
+        await login.login('standard_user', 'secret_sauce');
+        
+        // Should be in inventory page after login 
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    })
+
+    test('Invalid login gives error', async ({page}) => 
+    {
+        // Create a new object (login) 
+        const login = new LoginPage(page);
+        
+        // Goes to page and fills username and password fields
+        await login.gotoLoginPage();
+        await login.login('wrong_username', 'wrong_password');
+        
+        // Should give error message with specified message
+        await expect(login.errorMessage).toBeVisible()
+        await expect(login.errorMessage).toHaveText(
+            "Epic sadface: Username and password do not match any user in this service"
+        );
+    })
+})
